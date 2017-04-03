15e63f20b73f0357c6ade2f7ddd139cc23f7b625
SushmitaMondal001 <sushmita.mondal.bd@gmail.com>
2017-04-03 12:20:26 +1000

    Add project files.

---
+++ ENETCareWebFormApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ENETCareWebFormApp.Startup))]
+namespace ENETCareWebFormApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
