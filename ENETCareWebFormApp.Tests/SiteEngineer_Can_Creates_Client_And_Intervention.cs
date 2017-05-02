ba3ac2e42045a52a5a00e03d7a2ce440fbcc2b86
Syed Shean <shean.syed@gmail.com>
2017-05-02 18:30:35 +1000

    Syed-Unit Test Check

---
+++ ENETCareWebFormApp.Tests/SiteEngineer_Can_Creates_Client_And_Intervention.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ENETCareBusinessLogic;
+
+namespace ENETCareWebFormApp.Tests
+{
+    [TestClass]
+    public class SiteEngineer_Can_Creates_Client_And_Intervention
+    {
+        When_SiteEngineer_Creates_Intervention siteEngineer;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            siteEngineer = new When_SiteEngineer_Creates_Intervention();
+           
+        }
+
+        [TestMethod]
+        public void SiteEnineer_Can_Only_Propose_Intervention_When_Cost_Is_OutOfHis_Limit()
+        {
+            bool result = siteEngineer.Create_Proposed_Intervention("Proposed", 7200);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
